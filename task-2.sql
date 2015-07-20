41f52cc75d77b63e7541930d9e467de713addadf
Nikita Lyubchich <cybran111@gmail.com>
2015-07-20 21:24:07 +0300

    Added solution for task 2

---
+++ task-2.sql
@@ -0,0 +1,47 @@
+CREATE TABLE
+company (
+  id INT PRIMARY KEY,
+  name VARCHAR,
+  products_num INT
+);
+
+CREATE TABLE
+product (
+  id INT PRIMARY KEY,
+  title VARCHAR,
+  company_id INT REFERENCES company(id)
+);
+
+INSERT INTO company
+(id, name, products_num)
+VALUES
+(1, 'UAPROM', 0);
+
+CREATE OR REPLACE FUNCTION increment_products_num()
+  RETURNS trigger AS $$
+  BEGIN
+      UPDATE company SET products_num = products_num+1
+      WHERE company.id = NEW.company_id;
+      RETURN OLD;
+  END;
+$$ LANGUAGE plpgsql;
+
+CREATE OR REPLACE FUNCTION decrement_products_num()
+  RETURNS trigger AS $$
+  BEGIN
+      UPDATE company SET products_num = products_num-1
+      WHERE company.id = OLD.company_id;
+      RETURN OLD;
+  END;
+$$ LANGUAGE plpgsql;
+
+
+CREATE TRIGGER process_insert
+AFTER INSERT ON product
+FOR EACH ROW
+EXECUTE PROCEDURE increment_products_num();
+
+CREATE TRIGGER process_delete
+AFTER DELETE ON product
+FOR EACH ROW
+EXECUTE PROCEDURE decrement_products_num();
