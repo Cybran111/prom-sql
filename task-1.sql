beaa7dee015a3693c78dd1c91d9b69fffe9b4df0
n.lyubchich <cybran111@gmail.com>
2015-08-06 08:43:52 +0300

    Used GROUP BY instead of WHERE

--- task-1.sql
+++ task-1.sql
@@ -1,26 +1,25 @@
 -- DROP TABLE category;
 
 CREATE TABLE
-category (
-  id INT PRIMARY KEY,
-  parent_id INT REFERENCES category(id),
-  price INT NULL
+  category (
+  id        INT PRIMARY KEY,
+  parent_id INT REFERENCES category (id),
+  price     INT NULL
 );
 
 INSERT INTO category
- (id, parent_id, price)
-  VALUES
-   (1, null, 1),
-   (2, 1, null),
-   (3, 1, 2),
-   (4, 2, null),
-   (5, 4, 3),
-   (6, 3, 4),
-   (7, 3, null)
-;
+(id, parent_id, price)
+VALUES
+  (1, NULL, 1),
+  (2, 1, NULL),
+  (3, 1, 2),
+  (4, 2, NULL),
+  (5, 4, 3),
+  (6, 3, 4),
+  (7, 3, NULL);
 
 WITH RECURSIVE
- cat_price (id, parent_id, price) AS (
+    cat_price (id, parent_id, price) AS (
     SELECT
       c.id,
       c.parent_id,
@@ -36,7 +35,9 @@ WITH RECURSIVE
       JOIN cat_price ct ON ct.parent_id = c.id
     WHERE ct.price IS NULL
   )
-SELECT cat_price.id, cat_price.price
+SELECT
+  cat_price.id,
+  max(cat_price.price)
 FROM cat_price
-WHERE cat_price.price IS NOT NULL
+GROUP BY cat_price.id
 ORDER BY cat_price.id;
